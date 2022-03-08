55cb304f249088dc6bdf70adb5a51cf9cdc7e889
PAULA RIBEIRO <paulasalinor@gmail.com>
2022-03-07 22:40:16 -0300

    requisito 1 ok

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class NotFound extends Component {
+  render() {
+    return (
+      <div data-testid="page-not-found">
+        <h1>Ops! Page Not Found =(</h1>
+      </div>
+    );
+  }
+}
